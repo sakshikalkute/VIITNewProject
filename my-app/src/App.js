7698a19c983bba95c49384e8f9566cee5e611899
Sakshi Kalkute <sakshikalkute@gmail.com>
2025-07-30 16:13:23 +0530

    fourth commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import About from './components/About';
-import {Routes,Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import TextForm from './components/TextForm';
 import BMICal from './components/BMICal';
 import Home from './components/Home';
+
 function App() {
   return (
-    <>
-    <Navbar Home="Home" About="About" TextForm="TextForm" BMICal="BMI"></Navbar>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/About' element={<About/>}></Route>
-      <Route path='/TextForm' element={<TextForm/>}></Route>
-      <Route path='/BMICal' element={<BMICal/>}></Route>
-    </Routes>
-    </>
+    <BrowserRouter>
+      <Navbar Home="Home" About="About" TextForm="TextForm" BMICal="BMI" />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/TextForm' element={<TextForm />} />
+        <Route path='/BMICal' element={<BMICal />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App;
